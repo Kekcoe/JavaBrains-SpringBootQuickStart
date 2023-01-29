ac9ab804bcf02658e4f5f6ab386c2afb680f7867
kekcoe <kekcoe@gmail.com>
2023-01-29 22:11:48 +0300

    Added metod that return string

--- src/main/java/io/javabrains/springbootstater/hello/HelloController.java
+++ src/main/java/io/javabrains/springbootstater/hello/HelloController.java
@@ -1,8 +1,14 @@
 package io.javabrains.springbootstater.hello;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+	@RequestMapping
+	public String sayHi() {
+		return "HI!";
+	}
+	
 }
